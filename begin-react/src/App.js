748bec5ead1a1809ff02f268ea5bf8d6194923a8
somefood <iamseokjuhong@gmail.com>
2023-10-06 16:49:20 +0900

    배열에 항목 추가하기
    - 배열에 새 항목을 추가 할 때에는 이렇게 spread 연산자를 사용하거나, concat 을 사용하시면 됩니다.

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,16 +1,30 @@
-import React, {useRef} from "react";
+import React, {useRef, useState} from "react";
 import Hello from "./Hello";
 import Wrapper from "./Wrapper";
 import './App.css';
 import Counter from "./Counter";
 import InputSample from "./InputSample";
 import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 // 닫힌 태그는 꼭 존재해야함 또는 Auto Closing
 // 두 개 이상의 태그는 무조건 하나의 태그로 감싸져야 하기에 <></> (Fragment)로 감싸기!
 // JSX 내부에 자바스크립트 변수는 {} 안에 감싸서 보여주기
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
@@ -26,16 +40,35 @@ function App() {
       username: 'liz',
       email: 'liz@example.com'
     }
-  ];
+  ]);
 
   const nextId = useRef(4); // useRef(.current의 기본값)
-  const onCreate = () => {
-
+  const onCreate = e => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    // setUsers([...users, user]);
+    setUsers(users.concat(user));
+    
+    setInputs({
+      username: "",
+      email: ""
+    });
     nextId.current += 1;
   }
 
   return (
-    <UserList users={users}/>
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users}/>
+    </>
   );
 }
 
