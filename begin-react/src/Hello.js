db98032fa7ff90828a6bbe5829a8b1da22912a9a
somefood <iamseokjuhong@gmail.com>
2023-10-06 14:18:53 +0900

    컴포넌트 만들어 보기
    - ReactDOM.render 의 역할은 브라우저에 있는 실제 DOM 내부에 리액트 컴포넌트를 렌더링하겠다는 의미
    - public/index.html에 렌더링
    - 리액트 컴포넌트가 렌더링 될 때에는, 렌더링된 결과물이 위 div 내부에 렌더링되는 것 입니다.

---
+++ begin-react/src/Hello.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Hello() {
+  return <div>안녕하세요</div>
+}
+
+// Hello 컴포넌트 내보내서 다른 컴포넌트에서 불러와 사용할 수 있음
+export default Hello;
--- begin-react/src/index.js
+++ begin-react/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// ReactDOM.render 의 역할은 브라우저에 있는 실제 DOM 내부에 리액트 컴포넌트를 렌더링하겠다는 의미
+// public/index.html에 렌더링
+// 리액트 컴포넌트가 렌더링 될 때에는, 렌더링된 결과물이 위 div 내부에 렌더링되는 것 입니다.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
