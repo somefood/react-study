9d4d44b656a8005cd09ac9feee31c31a96ce03d3
somefood <iamseokjuhong@gmail.com>
2023-10-06 15:44:33 +0900

    input 상태 관리하기

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -3,13 +3,14 @@ import Hello from "./Hello";
 import Wrapper from "./Wrapper";
 import './App.css';
 import Counter from "./Counter";
+import InputSample from "./InputSample";
 
 // 닫힌 태그는 꼭 존재해야함 또는 Auto Closing
 // 두 개 이상의 태그는 무조건 하나의 태그로 감싸져야 하기에 <></> (Fragment)로 감싸기!
 // JSX 내부에 자바스크립트 변수는 {} 안에 감싸서 보여주기
 function App() {
   return (
-    <Counter/>
+    <InputSample/>
   );
 }
 
