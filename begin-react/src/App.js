872efb03f0a8eff79dc023d0eebdb07356b71543
somefood <iamseokjuhong@gmail.com>
2023-10-06 16:35:59 +0900

    useRef 로 컴포넌트 안의 변수 만들기
    - useRef 로 관리하는 변수는 값이 바뀐다고 해서 컴포넌트가 리렌더링되지 않습니다. 리액트 컴포넌트에서의 상태는 상태를 바꾸는 함수를 호출하고 나서 그 다음 렌더링 이후로 업데이트 된 상태를 조회 할 수 있는 반면, useRef 로 관리하고 있는 변수는 설정 후 바로 조회 할 수 있습니다.

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 import Hello from "./Hello";
 import Wrapper from "./Wrapper";
 import './App.css';
@@ -10,8 +10,32 @@ import UserList from "./UserList";
 // 두 개 이상의 태그는 무조건 하나의 태그로 감싸져야 하기에 <></> (Fragment)로 감싸기!
 // JSX 내부에 자바스크립트 변수는 {} 안에 감싸서 보여주기
 function App() {
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+
+  const nextId = useRef(4); // useRef(.current의 기본값)
+  const onCreate = () => {
+
+    nextId.current += 1;
+  }
+
   return (
-    <UserList/>
+    <UserList users={users}/>
   );
 }
 
